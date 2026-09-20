440c56e42602f3a756953b515e6e6236cbc49489
Adityaraval13 <adityaraval1306@gmail.com>
2026-09-20 12:48:40 +0530

    Day 2: Solved Valid Anagram

---
+++ dsa-practice/02-valid-anagram.js
@@ -0,0 +1,34 @@
+/*You are given two strings, s and t. You need to return true if t is an anagram of s,
+ and false if it is not.
+
+An anagram is a word formed by rearranging the letters of another word, 
+using every original letter exactly once.
+
+Example 1: s = "anagram", t = "nagaram" ➔ returns true
+
+Example 2: s = "rat", t = "car" ➔ returns false */
+
+function validAnagram(s, t) {
+  if (s.length !== t.length) return false;
+
+  s = s.toLowerCase();
+t = t.toLowerCase();
+
+  const count = {};
+
+  for (let char of s) {
+    count[char] = (count[char] || 0) + 1;
+  }
+  
+  for(let char of t){
+    if(!count[char] || count[char] === 0 ){
+        return false ;
+    }
+    count[char] -- ;
+  }
+
+  return true ;
+}
+
+const s = "rat", t = "car"
+console.log(validAnagram(s, t));
